37825c4d548214a4d1c05cbee4e2a15e1f9432de
hust-tzr <1144958114@qq.com>
2026-05-11 16:05:21 +0800

    说明：删除了test

--- text.py
+++
@@ -1,6 +0,0 @@
-from pix2text import Pix2Text
-
-img_fp = 'D:\个人资料及学科资料\myself\简历\唐卓然-个人简历.pdf'
-p2t = Pix2Text.from_config()
-doc = p2t.recognize_pdf(img_fp, page_numbers=[0, 1])
-doc.to_markdown('D:\个人资料及学科资料\myself\简历')  # 导出的 Markdown 信息保存在 output-md 目录中
